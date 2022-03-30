7210ce3db40d02bdefc9617e654c91c2aa4b5c38
Siddartha k <skoppara@its.jnj.com>
2022-03-31 02:50:53 +0530

    fixed data events

--- components/Event.tsx
+++ components/Event.tsx
@@ -27,7 +27,7 @@ export default function Event({ event, user }) {
         }>
         {/* <img className={"h-[136px] w-full object-cover"} src={`./assets/${eventImage}`} alt={eventName} /> */}
         <img className={"h-[136px] w-full object-cover"} src={`/assets/${image}`} alt={name} />
-        <div className=" h-[120px] w-full px-8 pt-4">
+        <div className=" h-[130px] w-full px-8 pt-4">
           <h3 className="text-2xl font-extrabold  text-[#272d67]">{name}</h3>
 
           <p className="mt-1 font-bold tracking-wide text-[#379392]">
